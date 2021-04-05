d455a1c06d5ffc8a7ecfe74176765c6987270f90
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-04-05 12:31:21 +0300

    refactor: add class to tag
    according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add('remove');
     deleteButton.appendChild(deleteButtonImg);
 
 
